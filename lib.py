fcfc522f2e04d119d84b3b132814860d0909729f
sunshineplan <sunshineplan@users.noreply.github.com>
2018-10-24 15:26:44 +0800

    Revert "if sensitive words don't contain @, they will highlight as string instead of regular expression expression"
    This reverts commit 49c37fd5458ea1abfa72d6c2f33824be4d0ec232.

--- lib.py
+++ lib.py
@@ -11,14 +11,15 @@ def check(article,filename):
     check_list = file2list(filename)
     if check_list == []:
         return ['Error!\nSensitive Words List is empty.\nPlease check your setting.'],['empty']
-    reg_keywords = [i.replace('@', '.?') for i in check_list if '@' in i]
-    string_keywords = [i for i in check_list if '@' not in i]
-    for i in reg_keywords:
-        p = re.compile(i)
+    keywords = [i.replace('@', '.?') for i in check_list]
+    reg = [i for i in check_list if '@' in i]
+    noreg = [i for i in check_list if '@' not in i]
+    for i in reg:
+        p = re.compile(i.replace('@', '.?'))
         n = len(p.findall(article))
         if n != 0:
-            result.append('[' + i.replace('.?', '@') + '] appears ' + str(n) + ' time(s).')
-    for i in string_keywords:
+            result.append('[' + i + '] appears ' + str(n) + ' time(s).')
+    for i in noreg:
         n = article.upper().count(i.upper())
         if n != 0:
             result.append('[' + i + '] appears ' + str(n) + ' time(s).')
@@ -27,7 +28,7 @@ def check(article,filename):
         result.append('Congratulations! No sensitive words were found.')
     else:
         result.insert(0, str(len(result)) + ' sensitive word(s) found:\n')
-    return result, reg_keywords, string_keywords
+    return result, keywords
 
 
 def file2list(filename):
